d4a5d81013ad60faca6045355a607491e57cc062
gcolomer <gcolomer@protonmail.com>
2025-07-17 09:24:57 +0200

    Amélioration de la gestion des WebSockets dans websockets.py. Ajout de vérifications de l'état du client avant l'envoi des logs et gestion des exceptions pour les déconnexions. Renforcement des logs d'erreur pour une meilleure traçabilité.

--- backend/src/routers/websockets.py
+++ backend/src/routers/websockets.py
@@ -1,5 +1,6 @@
 import asyncio
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from starlette.websockets import WebSocketState
 from src.core.logger import logger
 from src.core.exceptions import OpenSearchException
 from src.dto import LogResponse
@@ -15,11 +16,14 @@ async def last_logs(websocket: WebSocket):
             try:
                 logs: list[LogResponse] = websocket.app.state.\
                     opensearch_client.get_last_20_log()
-                await websocket.send_json([log.model_dump() for log in logs])
-                await asyncio.sleep(3)
-            except (Exception, OpenSearchException) as e:
+                if websocket.client_state == WebSocketState.CONNECTED:
+                    await websocket.send_json([log.model_dump() for log in logs])
+                    await asyncio.sleep(3)
+                else:
+                    logger.warning("Client websocket disconnected")
+            except (OpenSearchException) as e:
                 logger.error(f"Error: {e}")
-                await websocket.send_json({"error": str(e)})
-                await asyncio.sleep(10)
-    except WebSocketDisconnect:
-        logger.info("Client disconnected")
+    except WebSocketDisconnect as e:
+        logger.info(f"Websocket disconnected: {e}")
+    except Exception as e:
+        logger.error(f"Error: {e}")
